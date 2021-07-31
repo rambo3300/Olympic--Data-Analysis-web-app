9c787ab84e781c2686995d95a53ea43fda0c8578
Ritabrata2000 <chakrabortyritabrata8@gmail.com>
2021-08-01 01:26:01 +0530

    make it better

--- helper.py
+++ helper.py
@@ -2,25 +2,6 @@
 import numpy as np
 
 
-'''def medal_tally(df):
-    # dropping duplicates
-    medal_tally = df.drop_duplicates(subset= ['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
-    
-    # gruopby using NOC and sort it by Gold
-    medal_tally = medal_tally.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending = False).reset_index()
-    
-    # creating a column for total medals
-    medal_tally['Total'] = medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze'] 
-    
-    # converting no of medals from float to int 
-    medal_tally['Gold'] = medal_tally['Gold'].astype('int')
-    medal_tally['Silver'] = medal_tally['Silver'].astype('int')
-    medal_tally['Bronze'] = medal_tally['Bronze'].astype('int')
-    medal_tally['Total'] = medal_tally['Total'].astype('int')
-
-    return medal_tally'''
-
-
 def country_year_list(df):
     # getting all the name of countries 
     country = np.unique(df['region'].dropna()).tolist()
